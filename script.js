6c56c7528a9aa6548d12183b5c5457e5cb4cd546
mallebo <78452531+mallebo@users.noreply.github.com>
2021-06-22 20:25:14 +0000

    add name

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 // Author:
 
 // Global UI Variables
+let mobilenet;
+let video;
+let isModelReady;
 //let canvasDiv;
 
 // Global ML Variables
 //let mobilenet;
 
 function setup() {
+  video = createCapture(VIDEO);
+  video.style("display", "none");
+  isModelReady = false;
+  mobilenet = ml5.imageClassifier("MobileNet", modelReady);
+
+
+
+
 
 }
 
 function draw() {
+  image(video, 0, 0);
+  if(isModelReady) {
+  mobilenet.classify(canvas, gotResults);
+
+}
 
 }
 
 function modelReady() {
+  isModelReady = true;
+
 
 }
 
